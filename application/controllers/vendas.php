72f14f96ec4797a7d991dca311ec1230c93b02c3
EdersonLRF <edersonluis.rf@gmail.com>
2016-10-13 21:50:49 -0300

    2A10 enviando emails

--- application/controllers/vendas.php
+++ application/controllers/vendas.php
@@ -5,10 +5,11 @@ class Vendas extends CI_Controller {
     public function nova() {
         $usuario = autoriza();
 
-        // Carrega a model.
-        $this->load->model(array("vendas_model"));
+        // Carrega as models.
+        $this->load->model(array("vendas_model", "produtos_model", "usuarios_model"));
 
-        $this->load->helper(array("date"));
+        // Foi para o autoload.
+        // $this->load->helper(array("date"));
 
         $venda = array(
             "produto_id" => $this->input->post("produto_id"),
@@ -18,8 +19,35 @@ class Vendas extends CI_Controller {
 
         $this->vendas_model->salva($venda);
 
-        $this->session->set_flashdata("success", "Pedido de compra efetuado com sucesso!");
+        // Enviar e-mail.
+        $this->load->library("email");
 
+        $config["protocol"] = "smtp";
+        $config["smtp_host"] = "ssl://smtp.gmail.com";
+        $config["smtp_user"] = "codeigniteralura@gmail.com";
+        $config["smtp_pass"] = "123456";
+        $config["charset"] = "utf-8";
+        $config["mailtype"] = "html";
+        $config["newline"] = "\r\n";
+        $config["smtp_port"] = '465';
+        $this->email->initialize($config);
+
+        $produto = $this->produtos_model->busca($venda["produto_id"]);
+        $vendedor = $this->usuarios_model->busca($produto["usuario_id"]);
+
+        $dados = array("produto" => $produto);
+        // O terceiro parametro faz retornar o html e nao renderizar para o usuario.
+        $conteudo = $this->load->view("vendas/email.php", $dados, true);
+
+        $this->email->from("codeigniteralura@gmail.com", "Mercado");
+
+        $this->email->to($vendedor["email"]);
+
+        $this->email->subject("Seu produto {$produto['nome']} foi vendido!");
+        $this->email->message($conteudo);
+        $this->email->send();
+
+        $this->session->set_flashdata("success", "Pedido de compra efetuado com sucesso!");
         redirect("/");
     }
 
--- application/models/usuarios_model.php
+++ application/models/usuarios_model.php
@@ -16,4 +16,10 @@ class Usuarios_model extends CI_Model {
         return $usuario;
     }
 
+    public function busca($id) {
+        $this->db->where("id", $id);
+
+        return $this->db->get("usuarios")->row_array();
+    }
+
 }
