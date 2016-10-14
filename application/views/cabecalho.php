c5bb53fcacd0f2854d4ab4ec41e3776ee172012e
EdersonLRF <edersonluis.rf@gmail.com>
2016-10-13 22:29:41 -0300

    correcao

--- application/views/cabecalho.php
+++ application/views/cabecalho.php
@@ -6,10 +6,10 @@
 <body>
     <div class="container">
 
-        <?php if($this->session->flashdata("success")) : ?>
-            <p class="alert alert-success"><?= $this->session->flashdata("success") ?></p>
-        <?php endif ?>
+        <?php if ($this->session->flashdata("success")) : ?>
+            <p class="alert alert-success"><?= $this->session->flashdata("success"); ?></p>
+        <?php endif; ?>
 
-        <?php if($this->session->flashdata("danger")) : ?>
-            <p class="alert alert-danger"><?= $this->session->flashdata("danger") ?></p>
-        <?php endif ?>
+        <?php if ($this->session->flashdata("danger")) : ?>
+            <p class="alert alert-danger"><?= $this->session->flashdata("danger"); ?></p>
+        <?php endif; ?>
